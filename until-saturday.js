125254212f08569e64bd0f211e64f1b47e8d15f9
hparker13 <heather@steamdrake.com>
2022-07-13 10:28:19 -0400

    Added until-saturday.js

---
+++ until-saturday.js
@@ -0,0 +1,24 @@
+let day = 'Wednesday';
+
+switch (day) {
+    case 'Sunday':
+        console.log('There are 6 days until Saturday.');
+        break;
+    case 'Monday':
+        console.log('There are 5 days until Saturday.');
+        break;    
+    case 'Tuesday':
+        console.log('There are 4 days until Saturday.');
+        break;
+    case 'Wednesday':
+        console.log('There are 3 days until Saturday.');
+        break;
+    case 'Friday':
+        console.log('There is 1 day until Saturday.');
+        break;
+    case 'Saturday':
+        console.log('It is Saturday!');
+        break;
+    default:
+        console.log('That is not a day of the week.  Try again.')
+}
